0405fb7aac6d20b068989b5dd7caad85fc69fe7b
Luckson456 <lpaija@flemingcollege.ca>
2024-06-12 20:55:48 -0400

    completed  group order by ID AND ITERATE

--- process_sales_data.py
+++ process_sales_data.py
@@ -72,17 +72,33 @@ def process_sales_data(sales_csv_path, orders_dir_path):
     # TODO: Insert a new "TOTAL PRICE" column into the DataFrame
     sales_df.insert(7, 'Total Price',sales_df['Item Quantity']*sales_df['Item Price'])
     # TODO: Remove columns from the DataFrame that are not needed
-    sales_df.drop(columns=['ADDRESS','CITY','STATE','POSTAL CODE','COUNTRY']inplace =True)
+    sales_df.drop(columns=['ADDRESS','CITY','STATE','POSTAL CODE','COUNTRY'],inplace =True)
     # TODO: Groups orders by ID and iterate 
+    for order_id,order_df in sales_df.groupby('ORDER ID'):
         # TODO: Remove the 'ORDER ID' column
+        order_df =order_df.drop(columns=['ORDER ID'])
         # TODO: Sort the items by item number
+        order_df=order_df.sort_values('Item Number')
         # TODO: Append a "GRAND TOTAL" row
+        grand_total =order_df['Total Price'].sum()
+        grand_total_row=pd.DataFrame({'Item number':['Grand Total'],'Item Quantity':['-'],'Item Price':['-'],'Total Price':[grand_total]})
+        order_df=pd.concat([order_df,grand_total_row],ignore_index=True)
         # TODO: Determine the file name and full path of the Excel sheet
+        order_file_name =f"order_{order_id}.xlsx"
+        order_file_path =os.path.join(orders_dir_path,order_file_name)     
         # TODO: Export the data to an Excel sheet
+        with pd.ExcelWriter(order_file_path) as writer:
+            order_df.to_excel(writer,index=False, sheet_name=f"Order{order_id}")
         # TODO: Format the Excel sheet
+        worksheet=writer.sheets[f"Order{order_id}"]
         # TODO: Define format for the money columns
+        money_format ='${:,2f}'
         # TODO: Format each colunm
+        worksheet.set_column('C:D',15,writer.book.add_format({'num_format':money_format}))
+        worksheet.set_column('E:E',15,writer.book .add_format({'num_format':money_format}))
         # TODO: Close the Excelwriter 
+        writer.close()
+
     return
 
 if __name__ == '__main__':
